84520419acd70ea84bdca916cdeb026f6cc95f08
Marten de Jong <87567797+mg-de-j@users.noreply.github.com>
2025-12-07 16:25:07 +0100

    Refactor conversation flows and responses for user interactions

--- actions/actions.py
+++ actions/actions.py
@@ -5,19 +5,19 @@ from rasa_sdk.events import SlotSet
 from rasa_sdk.executor import CollectingDispatcher
 
 
-class ActionCheckSufficientFunds(Action):
-    def name(self) -> Text:
-        return "action_check_sufficient_funds"
+# class ActionCheckSufficientFunds(Action):
+#     def name(self) -> Text:
+#         return "action_check_sufficient_funds"
 
-    def run(
-        self,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any],
-    ) -> List[Dict[Text, Any]]:
-        # hard-coded balance for tutorial purposes. in production this
-        # would be retrieved from a database or an API
-        balance = 1000
-        transfer_amount = tracker.get_slot("amount")
-        has_sufficient_funds = transfer_amount <= balance
-        return [SlotSet("has_sufficient_funds", has_sufficient_funds)]
+#     def run(
+#         self,
+#         dispatcher: CollectingDispatcher,
+#         tracker: Tracker,
+#         domain: Dict[Text, Any],
+#     ) -> List[Dict[Text, Any]]:
+#         # hard-coded balance for tutorial purposes. in production this
+#         # would be retrieved from a database or an API
+#         balance = 1000
+#         transfer_amount = tracker.get_slot("amount")
+#         has_sufficient_funds = transfer_amount <= balance
+#         return [SlotSet("has_sufficient_funds", has_sufficient_funds)]
